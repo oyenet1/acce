a785f30b59938e19c989d4c81d33e9180c468eb1
12345abuja <christyseeker@gmail.com>
2020-12-10 13:21:31 +0100

    ade

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
 
 
@@ -23,5 +23,24 @@ Route::group(['prefix' => 'admin'], function () {
 });
 
 Auth::routes();
+Route::view('/contact', 'contact')->name('contact');
+Route::get('/admission', [App\Http\Controllers\AdmissionController::class, 'index'])->name('admission');
+Route::post('/admission', [App\Http\Controllers\AdmissionController::class, 'store'])->name('admission.store');
+Route::post('/contact', [App\Http\Controllers\ContactController::class, 'store'])->name('contact.store');
+Route::view('/slider', 'slider')->name('slider');
+Route::view('/message', 'director')->name('message.director');
+Route::view('/mission', 'mission')->name('mission');
+Route::view('/beleive', 'beleive')->name('beleive');
+Route::view('/values', 'values')->name('values');
+Route::view('/goals', 'goals')->name('goals');
+Route::view('/search', 'search')->name('search');
+Route::view('/recruit', 'recruit')->name('recruit');
+Route::view('/career', 'search')->name('career');
+Route::view('/apply', 'apply')->name('apply');
+Route::view('/culture', 'culture')->name('culture');
+Route::view('/join', 'join')->name('join');
+Route::view('/coming', 'coming')->name('coming');
+Route::view('/photo', 'photo')->name('photo');
+Route::view('/payment', 'payment')->name('payment');
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
